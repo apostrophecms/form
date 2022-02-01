5bc2367656fcca8bac8d8ee28d1cca05ec3130a2
Alex Bea <abea@mailbox.org>
2022-02-01 14:04:16 -0600

    Spacing cleanup

--- index.js
+++ index.js
@@ -240,120 +240,120 @@ module.exports = {
     return {
       post: {
         // Route to accept the submitted form.
-        submit:
-      [
-        require('connect-multiparty')(),
-        async function (req) {
-          const input = JSON.parse(req.body.data);
-          const output = {};
-          const formErrors = [];
-          const formId = self.inferIdLocaleAndMode(req, input._id);
+        submit: [
+          require('connect-multiparty')(),
+          async function (req) {
+            const input = JSON.parse(req.body.data);
+            const output = {};
+            const formErrors = [];
+            const formId = self.inferIdLocaleAndMode(req, input._id);
 
-          const form = await self.find(req, {
-            _id: self.apos.launder.id(formId)
-          }).toObject();
+            const form = await self.find(req, {
+              _id: self.apos.launder.id(formId)
+            }).toObject();
 
-          if (!form) {
-            throw self.apos.error('notfound');
-          }
-
-          if (form.enableRecaptcha) {
-            try {
-            // Process reCAPTCHA input if needed.
-              await self.checkRecaptcha(req, input, formErrors);
-            } catch (e) {
-              self.apos.util.error('reCAPTCHA submission error', e);
-              throw self.apos.error('invalid');
+            if (!form) {
+              throw self.apos.error('notfound');
             }
-          }
 
-          // Find any file field submissions and insert the files as attachments
-          for (const [ field, value ] of Object.entries(input)) {
-            if (value === 'files-pending') {
+            if (form.enableRecaptcha) {
               try {
-                input[field] = await uploadFieldFiles(req, field, req.files, {
-                  insert: self.apos.attachment.insert,
-                  warn: self.apos.util.warn
-                });
-              } catch (error) {
-                self.apos.util.error(error);
-                formErrors.push({
-                  field: field,
-                  error: 'invalid',
-                  message: req.t('aposForm:fileUploadError')
-                });
+                // Process reCAPTCHA input if needed.
+                await self.checkRecaptcha(req, input, formErrors);
+              } catch (e) {
+                self.apos.util.error('reCAPTCHA submission error', e);
+                throw self.apos.error('invalid');
               }
             }
-          }
 
-          // Recursively walk the area and its sub-areas so we find
-          // fields nested in two-column widgets and the like
-
-          // walk is not an async function so build an array of them to start
-          const areas = [];
-
-          self.apos.area.walk({
-            contents: form.contents
-          }, function(area) {
-            areas.push(area);
-          });
-
-          const fieldNames = [];
-          const conditionals = {};
-          const skipFields = [];
-
-          // Populate the conditionals object fully to clear disabled values
-          // before starting sanitization.
-          for (const area of areas) {
-            const widgets = area.items || [];
-            for (const widget of widgets) {
-            // Capture field names for the params check list.
-              fieldNames.push(widget.fieldName);
-
-              if (widget.type === '@apostrophecms/form-conditional') {
-                trackConditionals(conditionals, widget);
-              }
-            }
-          }
-
-          collectToSkip(input, conditionals, skipFields);
-
-          for (const area of areas) {
-            const widgets = area.items || [];
-            for (const widget of widgets) {
-              const manager = self.apos.area.getWidgetManager(widget.type);
-              if (
-                manager && manager.sanitizeFormField &&
-                !skipFields.includes(widget.fieldName)
-              ) {
+            // Find any file field submissions and insert the files as attachments
+            for (const [ field, value ] of Object.entries(input)) {
+              if (value === 'files-pending') {
                 try {
-                  manager.checkRequired(req, widget, input);
-                  await manager.sanitizeFormField(widget, input, output);
-                } catch (err) {
-                  if (err.data && err.data.fieldError) {
-                    formErrors.push(err.data.fieldError);
-                  } else {
-                    throw err;
+                  input[field] = await uploadFieldFiles(req, field, req.files, {
+                    insert: self.apos.attachment.insert,
+                    warn: self.apos.util.warn
+                  });
+                } catch (error) {
+                  self.apos.util.error(error);
+                  formErrors.push({
+                    field: field,
+                    error: 'invalid',
+                    message: req.t('aposForm:fileUploadError')
+                  });
+                }
+              }
+            }
+
+            // Recursively walk the area and its sub-areas so we find
+            // fields nested in two-column widgets and the like
+
+            // walk is not an async function so build an array of them to start
+            const areas = [];
+
+            self.apos.area.walk({
+              contents: form.contents
+            }, function(area) {
+              areas.push(area);
+            });
+
+            const fieldNames = [];
+            const conditionals = {};
+            const skipFields = [];
+
+            // Populate the conditionals object fully to clear disabled values
+            // before starting sanitization.
+            for (const area of areas) {
+              const widgets = area.items || [];
+              for (const widget of widgets) {
+                // Capture field names for the params check list.
+                fieldNames.push(widget.fieldName);
+
+                if (widget.type === '@apostrophecms/form-conditional') {
+                  trackConditionals(conditionals, widget);
+                }
+              }
+            }
+
+            collectToSkip(input, conditionals, skipFields);
+
+            for (const area of areas) {
+              const widgets = area.items || [];
+              for (const widget of widgets) {
+                const manager = self.apos.area.getWidgetManager(widget.type);
+                if (
+                  manager && manager.sanitizeFormField &&
+                !skipFields.includes(widget.fieldName)
+                ) {
+                  try {
+                    manager.checkRequired(req, widget, input);
+                    await manager.sanitizeFormField(widget, input, output);
+                  } catch (err) {
+                    if (err.data && err.data.fieldError) {
+                      formErrors.push(err.data.fieldError);
+                    } else {
+                      throw err;
+                    }
                   }
                 }
               }
             }
+
+            if (formErrors.length > 0) {
+              throw self.apos.error('invalid', {
+                formErrors
+              });
+            }
+
+            if (form.enableQueryParams && form.queryParamList.length > 0) {
+              self.processQueryParams(form, input, output, fieldNames);
+            }
+
+            await self.emit('submission', req, form, output);
+
+            return {};
           }
-
-          if (formErrors.length > 0) {
-            throw self.apos.error('invalid', {
-              formErrors
-            });
-          }
-
-          if (form.enableQueryParams && form.queryParamList.length > 0) {
-            self.processQueryParams(form, input, output, fieldNames);
-          }
-
-          await self.emit('submission', req, form, output);
-
-          return {};
-        } ]
+        ]
       }
     };
   },
